97dfe3438e227aece2f788db45ccc0f92fb17087
akhildac <54618450+akhildac@users.noreply.github.com>
2019-08-30 01:25:40 -0700

    Add files via upload

---
+++ simpleInterest.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	float principle,rate,time,simpleInterest;
+	cout<<"Enter Principle Amount"<<endl;
+	cin>>principle;
+	cout<<"Enter Rate of Intrest"<<endl;
+	cin>>rate;
+	cout<<"Enter Time Period"<<endl;
+	cin>>time;
+	simpleInterest=(principle*time*rate)/100;
+	cout<<"The SIMPLE INEREST is\t"<<simpleInterest;
+	
+
+
+
+
+
+}
