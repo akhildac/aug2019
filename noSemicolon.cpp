ca8baaf3125b6074f22fcc8fb94f2edc5d74be12
akhildac <54618450+akhildac@users.noreply.github.com>
2019-08-30 01:25:06 -0700

    Add files via upload

---
+++ noSemicolon.cpp
@@ -0,0 +1,11 @@
+
+#define N 10
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int num) {
+	if(num<=N && cout<<num<<endl && main(num+1)){
+		
+	}
+}
