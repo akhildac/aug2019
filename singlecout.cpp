a67c6b66fa44f065e49dbb876fb093deb56dfe12
akhildac <54618450+akhildac@users.noreply.github.com>
2019-08-30 01:26:19 -0700

    Add files via upload

---
+++ singlecout.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	cout<<"Subject\t"<<"\tMarks\n"<<"Mathematics\t"<<"90\n"<<"Computer\t"<<"77\n"<<"Chemistry\t"<<"69";
+}
